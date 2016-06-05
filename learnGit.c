630c3a8ad924e95507811092257b068f732decef
xmoyKing <822547462@qq.com>
2016-06-05 20:29:26 +0800

    append GPL

--- learnGit.c
+++ learnGit.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-	char s[]="Git is a version control system, also a free software.";
+	char s[]="Git is a version control system, also a free software under GPL.";
 	char s1[]="Git is a distributed version control system.";
 	return 0;
 }
