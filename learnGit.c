82c6bafaa697f3d8ffb03711d50e85deb1fec267
xmoyKing <822547462@qq.com>
2016-06-05 20:48:56 +0800

    git tracks changes

--- learnGit.c
+++ learnGit.c
@@ -5,6 +5,8 @@ int main(){
 	char s1[]="Git is a distributed version control system.";
 	
 	char s2[]="Git has a mutable index called stage";
+	
+	char s3[]="Git tracks changes of files";
 	return 0;
 
 }
