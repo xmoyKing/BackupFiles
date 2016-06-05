1a6abdd9c602403418844f4209202690d506bcba
xmoyKing <822547462@qq.com>
2016-06-05 20:43:01 +0800

    understand how stage works

--- learnGit.c
+++ learnGit.c
@@ -3,5 +3,8 @@
 int main(){
 	char s[]="Git is a version control system, also a free software under GPL.";
 	char s1[]="Git is a distributed version control system.";
+	
+	char s2[]="Git has a mutable index called stage";
 	return 0;
+
 }
